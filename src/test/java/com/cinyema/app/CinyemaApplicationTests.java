8f94cbebc2071a610143365dd430c9e08d780bb9
VieDev <piri.viejo@gmail.com>
2021-10-25 21:53:48 -0300

    Armamos proyecto

---
+++ src/test/java/com/cinyema/app/CinyemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinyema.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinyemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

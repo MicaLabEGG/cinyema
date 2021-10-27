bca9b76751fdd82c94d4298504d52b535ca36fd0
Micael Labria <micaelcapo1316@gmail.com>
2021-10-26 22:00:30 -0300

    Actor creado, con Pais enum

--- src/main/java/com/cinyema/app/Entidad/Actor.java
+++ src/main/java/com/cinyema/app/Entidad/Actor.java
@@ -1,5 +1,68 @@
 package com.cinyema.app.Entidad;
 
-public class Actor {
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import com.cinyema.app.enumeracion.Pais;
 
+@Entity
+public class Actor {
+	@Id
+	private Long idActor = randomId();
+	@Column(unique = true)
+	private String nombreCompleto;
+	@Enumerated(EnumType.STRING)
+	private Pais pais;
+	
+	public Actor(Long idActor, String nombreCompleto, Pais pais) {
+		super();
+		this.idActor = idActor;
+		this.nombreCompleto = nombreCompleto;
+		this.pais = pais;
+	}
+
+	public Actor() {
+		super();
+	}
+
+	public Long getIdActor() {
+		return idActor;
+	}
+
+	public void setIdActor(Long idActor) {
+		this.idActor = idActor;
+	}
+
+	public String getNombreCompleto() {
+		return nombreCompleto;
+	}
+
+	public void setNombreCompleto(String nombreCompleto) {
+		this.nombreCompleto = nombreCompleto;
+	}
+
+	public Pais getPais() {
+		return pais;
+	}
+
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+	
+	public Long randomId() {
+		String uuid = UUID.randomUUID().toString();
+		Long id = (long) uuid.hashCode();
+		id = id<0 ? -id:id;
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Actor [idActor=" + idActor + ", nombreCompleto=" + nombreCompleto + ", pais=" + pais + "]";
+	}
+
+	
 }

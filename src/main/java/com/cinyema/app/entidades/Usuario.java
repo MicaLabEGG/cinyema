459758305b3742674d92fbcad07a78e4e69e0ef3
Alex <alextr_r@outlook.com>
2021-10-28 18:33:30 -0300

    Corrección entidad Usuario

--- src/main/java/com/cinyema/app/entidades/Usuario.java
+++ src/main/java/com/cinyema/app/entidades/Usuario.java
@@ -1,5 +1,6 @@
 package com.cinyema.app.entidades;
 
+import java.sql.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -10,6 +11,8 @@ import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import com.cinyema.app.enumeraciones.Rol;
 
@@ -25,6 +28,9 @@ public class Usuario {
 	private String nombreDeUsuario;
 	private String contraseña;
 	private Boolean alta;
+	
+	@Temporal(TemporalType.DATE)
+	private Date fechaNacimiento;
 	
 	@Enumerated(EnumType.STRING)
 	private Rol rol;
@@ -47,76 +53,79 @@ public class Usuario {
 		this.rol = rol;
 	}
 
-
-	public long getIdUsuario() {
+	
+	
+	public Long getIdUsuario() {
 		return idUsuario;
 	}
 
-
-	public void setIdUsuario(long idUsuario) {
+	public void setIdUsuario(Long idUsuario) {
 		this.idUsuario = idUsuario;
 	}
-
 
 	public String getNombre() {
 		return nombre;
 	}
 
-	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
 
 	public String getMail() {
 		return mail;
 	}
 
-
 	public void setMail(String mail) {
 		this.mail = mail;
 	}
-
 
 	public String getNombreDeUsuario() {
 		return nombreDeUsuario;
 	}
 
-
 	public void setNombreDeUsuario(String nombreDeUsuario) {
 		this.nombreDeUsuario = nombreDeUsuario;
 	}
-
 
 	public String getContraseña() {
 		return contraseña;
 	}
 
-
 	public void setContraseña(String contraseña) {
 		this.contraseña = contraseña;
 	}
-
 
 	public Boolean getAlta() {
 		return alta;
 	}
 
-
 	public void setAlta(Boolean alta) {
 		this.alta = alta;
 	}
 
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
 
 	public Rol getRol() {
 		return rol;
 	}
 
-
 	public void setRol(Rol rol) {
 		this.rol = rol;
 	}
 
+	public List<Ticket> getTicket() {
+		return ticket;
+	}
+
+	public void setTicket(List<Ticket> ticket) {
+		this.ticket = ticket;
+	}
 
 	public Long randomId() {
 		String uuid = UUID.randomUUID().toString();
@@ -125,12 +134,15 @@ public class Usuario {
 		return id;
 	}
 
-
 	@Override
 	public String toString() {
 		return "Usuario [idUsuario=" + idUsuario + ", nombre=" + nombre + ", mail=" + mail + ", nombreDeUsuario="
-				+ nombreDeUsuario + ", contraseña=" + contraseña + ", alta=" + alta + ", rol=" + rol + "]";
+				+ nombreDeUsuario + ", contraseña=" + contraseña + ", alta=" + alta + ", fechaNacimiento="
+				+ fechaNacimiento + ", rol=" + rol + ", ticket=" + ticket + "]";
 	}
+
+
+	
 
 	
 }

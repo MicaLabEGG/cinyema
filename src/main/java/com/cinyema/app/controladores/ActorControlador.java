19c2ed180f2ad7ea352d9578cfe4cfe3a9f5aacd
Oriana <orilupellegrini@gmail.com>
2021-10-30 01:20:58 -0300

    Conecto con los controladores las vistas de listar de la entidad actor y director

--- src/main/java/com/cinyema/app/controladores/ActorControlador.java
+++ src/main/java/com/cinyema/app/controladores/ActorControlador.java
@@ -16,62 +16,62 @@ import com.cinyema.app.servicios.ActorServicio;
 @Controller
 @RequestMapping("/actor")
 public class ActorControlador {
-	 
+
 	@Autowired
 	private ActorServicio actorServicio;
-	
-	@GetMapping()
+
+	@GetMapping("")
 	public String lista(ModelMap modelo) {
-		
+
 		List<Actor> todosActores = actorServicio.buscarActores();
-
 		modelo.addAttribute("actores", todosActores);
-
-		return "lista-actor";
+		return "admin/vistas/actor";
 	}
-	
+
 	@GetMapping("/ingreso")
 	public String ingreso() {
 		return "ingreso-actor";
 	}
-	
+
 	@PostMapping("/ingreso")
-	public String registrarActor(ModelMap modelo, @RequestParam String nombreCompleto, @RequestParam Pais pais) throws Exception  {
+	public String registrarActor(ModelMap modelo, @RequestParam String nombreCompleto, @RequestParam Pais pais)
+			throws Exception {
 		try {
-		      actorServicio.registrarActor(nombreCompleto,pais);
-		      modelo.put("exito", "Ingreso exitoso!");
-		      return "redirect:/actor";
-		}catch(Exception e) {
-              System.out.println(e.getMessage());
-              System.out.println(e.getStackTrace());
-			  modelo.put("error", "Falta algun dato o no puede ingresar un Actor con igual nombre a otro");
-			  return "ingreso-actor";
+			actorServicio.registrarActor(nombreCompleto, pais);
+			modelo.put("exito", "Ingreso exitoso!");
+			return "redirect:/actor";
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getStackTrace());
+			modelo.put("error", "Falta algun dato o no puede ingresar un Actor con igual nombre a otro");
+			return "ingreso-actor";
 		}
 	}
-	
+
 	@GetMapping("/modificar/{id}")
 	public String modificarActor(ModelMap modelo, @PathVariable Long id) throws Exception {
 		try {
-		     Actor actor = actorServicio.obtenerActor(id);
-		     modelo.addAttribute("actor",actor);
-		     return "modificar-actor";
-		}catch(Exception e) {
+			Actor actor = actorServicio.obtenerActor(id);
+			modelo.addAttribute("actor", actor);
+			return "modificar-actor";
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			modelo.put("error", "falta algun dato");
 			return "modificar-actor";
 		}
 	}
-	
+
 	@PostMapping("/modificar/{id}")
-	public String modificar(ModelMap modelo, @PathVariable Long id, @RequestParam String nombreCompleto, @RequestParam Pais pais) throws Exception {
+	public String modificar(ModelMap modelo, @PathVariable Long id, @RequestParam String nombreCompleto,
+			@RequestParam Pais pais) throws Exception {
 		try {
-		     actorServicio.modificarActor(id, nombreCompleto,pais);
-		     modelo.put("exito", "Ingreso exitoso!");
-		     return "redirect:/actor";
-		}catch(Exception e) {
-			 System.out.println(e.getMessage());
-			 e.printStackTrace();
-			 modelo.addAttribute("error", e.getMessage());
+			actorServicio.modificarActor(id, nombreCompleto, pais);
+			modelo.put("exito", "Ingreso exitoso!");
+			return "redirect:/actor";
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			modelo.addAttribute("error", e.getMessage());
 		}
 		return "redirect:/actor";
 	}

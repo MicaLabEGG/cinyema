72c01cebe53aaeb095648ff62d965fc1828cdbd6
Micael Labria <micaelcapo1316@gmail.com>
2021-11-02 17:14:25 -0300

    Creo que ahi quedo el controlador de Ticket funcional

--- src/main/java/com/cinyema/app/controladores/TicketControlador.java
+++ src/main/java/com/cinyema/app/controladores/TicketControlador.java
@@ -2,7 +2,6 @@ package com.cinyema.app.controladores;
 
 import java.util.Date;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -11,12 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import com.cinyema.app.entidades.Actor;
 import com.cinyema.app.entidades.Pelicula;
 import com.cinyema.app.entidades.Ticket;
 import com.cinyema.app.entidades.Usuario;
-import com.cinyema.app.enumeraciones.Pais;
 import com.cinyema.app.servicios.TicketServicio;
 
 
@@ -60,48 +56,42 @@ public class TicketControlador {
 		}
 	}
 	
-	
-
-	@GetMapping("/{id}")
-	public String vistaticket(ModelMap modelo, @PathVariable Long id) {
-		Ticket tic = servTic.buscarxId(id);
-		modelo.put("ticket", tic);
-		
-		return "ticket.html";		
-	}
-	
-	@GetMapping("/{id}/modificar")
+	@GetMapping("/modificar/{id}")
 	public String modificarTicket(ModelMap modelo, @PathVariable Long id) throws Exception {
 		try {
 		     Ticket ticket = servTic.buscarxId(id);
-		     modelo.addAttribute("ticket",ticket);
-		     return "modificarTicket";
+		     modelo.addAttribute("editar", "Editar Ticket");
+		     modelo.addAttribute("ticket", ticket);
+		     return "admin/vistas/ticket";
 		}catch(Exception e) {
 			System.out.println(e.getMessage());
 			modelo.put("error", "falta algun dato");
-			return "modificarTicket";
+			return "admin/vistas/ticket";
 		}
 	}
 	
-	@PostMapping("/{id}/modificar")
-	public String modificarTicket(ModelMap modelo, @PathVariable Long id, @RequestParam Pelicula pelicula, @RequestParam Usuario usuario, @RequestParam Date fecha, @RequestParam String lugar, @RequestParam Double precio) throws Exception {
+	@PostMapping("modificar/{id}")
+	public String modificarTicket(ModelMap modelo, @PathVariable Long id, @RequestParam Pelicula pelicula, @RequestParam Usuario usuario, @RequestParam String fecha, @RequestParam String lugar, @RequestParam Double precio) throws Exception {
 		try {
 		     servTic.modificarTicket(id, pelicula, usuario, fecha, lugar, precio);
-		     modelo.put("exito", "Modificación exitosa!");
-		     return "redirect:/ticket/{id}";
+		     return "redirect:/ticket";
 		}catch(Exception e) {
 			 System.out.println(e.getMessage());
 			 e.printStackTrace();
 			 modelo.addAttribute("error", e.getMessage());
 		}
-		return "redirect:/ticket/{id}";
+		return "redirect:/ticket";
 	}
 	
-	@GetMapping("/{id}/eliminar")
+	@GetMapping("/eliminar/{id}")
 	public String eliminarrTicket(@PathVariable Long id) {
-		
-		servTic.eliminarTicket(id);		
-		return "redirect:/autores";
+		try {
+		     servTic.eliminarTicket(id);		
+		     return "redirect:/ticket";
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+			return "redirect:/ticket";
+		}
 	}
 }
 	

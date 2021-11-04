d606d1ec8f9736346f8f09f781202c6c041acf5c
Micael Labria <micaelcapo1316@gmail.com>
2021-11-04 16:20:29 -0300

    Hecho el Repositorio de Cine y el Servicio de Cine (No se si esta bien) (Sin Terminar)

--- src/main/java/com/cinyema/app/servicios/CineServicio.java
+++ src/main/java/com/cinyema/app/servicios/CineServicio.java
@@ -1,9 +1,13 @@
 package com.cinyema.app.servicios;
 
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import com.cinyema.app.entidades.Cine;
+import com.cinyema.app.entidades.Sala;
 import com.cinyema.app.repositorios.CineRepositorio;
 
 @Service
@@ -12,13 +16,65 @@ public class CineServicio {
 	@Autowired
 	private CineRepositorio cineRepositorio;
 	
+	//@Autowired
+	//private SalaRepositorio salaRepositorio;
+	
 	@Transactional
-	public Cine crearCine(Cine cine) throws Exception{
+	public Cine crear(Cine cine) throws Exception{
 		
 		validar(cine);
 		
 		return cineRepositorio.save(cine);
+	}
+	
+	//Arme el modificar segun la nueva manera, que recibe el cine completo, por la vista con th:object para hacer los cambios.
+	//No se, si esta bien ??
+	@Transactional
+	public Cine modificar(Cine cine) throws Exception{
 		
+		//@SuppressWarnings({ "deprecation", "unchecked" })
+		//List<Sala> salas = (List<Sala>) salaRepositorio.getOne(cine.getSalas().get(0).getIdSala());
+		
+		validar(cine);
+		
+		Optional<Cine> respuesta = cineRepositorio.findById(cine.getIdCine());
+		
+		if (respuesta.isPresent()) {
+			Cine cine1 = respuesta.get();
+			if (cine1.getIdCine().equals(cine.getIdCine())) {
+				return cineRepositorio.save(cine1);
+			}else {
+				throw new Exception("*No puede realizar la modificacion");
+			}		
+		}else {
+			throw new Exception("No se encontró el Cine solicitado");
+		}
+	}	
+	
+	@Transactional
+	public List<Cine> listar() {	
+		return cineRepositorio.findAll();
+	}
+	
+	@Transactional
+	public List<Cine> listarCinePorNombre(String nombre) throws Exception {
+        List<Cine> cines = cineRepositorio.buscarCinePorNombre(nombre);
+		
+		if(!cines.isEmpty()) {
+			return cines;
+		}else {
+			throw new Exception("*No se encontró el nombre del Cine");
+		}	
+	}
+	
+	@Transactional
+	public Optional<Cine> buscarCinePorId(Long idCine) {
+		return cineRepositorio.findById(idCine);
+	}
+	
+	@Transactional
+	public void eliminarCine(Cine cine) {
+		cineRepositorio.delete(cine);
 	}
 	
 	public void validar(Cine cine) throws Exception {

7789114a67162f9cc2811cc8121b531504332893
Oriana <orilupellegrini@gmail.com>
2021-11-05 01:00:00 -0300

    Remodelando controlador ticket

--- src/main/java/com/cinyema/app/controladores/TicketControlador.java
+++ src/main/java/com/cinyema/app/controladores/TicketControlador.java
@@ -21,7 +21,6 @@ import com.cinyema.app.servicios.TicketServicio;
 import com.cinyema.app.servicios.UsuarioServicio;
 
 @Controller
-//para saber si inicio sesion(esta logueado) no tiene que ver con Roles - PreAhutorize
 @PreAuthorize("isAuthenticated()")
 @RequestMapping("/ticket")
 public class TicketControlador {
@@ -37,53 +36,51 @@ public class TicketControlador {
 
 	@PreAuthorize("hasAnyRole('ROLE_ADMINISTRADOR')")
 	@GetMapping("")
-	public String listaTicket(ModelMap modelo) {
-
+	public String listar(ModelMap modelo) {
 		try {
-			List<Ticket> listTickets = servicioTicket.listarTicket();
-			modelo.addAttribute("listar", "Lista de Tickets");
-			modelo.addAttribute("tickets", listTickets);
-			return "admin/vistas/ticket";
+			modelo.addAttribute("listar", "Lista Tickets");
+			modelo.addAttribute("tickets", servicioTicket.listarTicket());
+			return "vistas/ticket";
 		} catch (Exception e) {
-			e.getMessage();
-			return "admin/vistas/ticket";
+			e.printStackTrace();
+			return "vistas/ticket";
 		}
 	}
 
 	@PreAuthorize("hasAnyRole('ROLE_ADMINISTRADOR')")
 	@GetMapping("/registrar")
-	public String registrarTicket(ModelMap modelo) {
+	public String registrar(ModelMap modelo) {
 		try {
 			modelo.addAttribute("registrar", "Registrar Ticket");
 			modelo.addAttribute("ticket", servicioTicket.crearTicketVac());
 			modelo.addAttribute("peliculas", servicioPelicula.listarPeliculas());
 			modelo.addAttribute("usuarios", servicioUsuario.buscarUsuarios());
-			return "admin/vistas/ticket";
+			return "vistas/ticket";
 		} catch (Exception e) {
-			modelo.put("error", e.getMessage());
-			return "admin/vistas/ticket";
+			e.printStackTrace();
+			modelo.put("error", "Error al ingresar datos");
+			return "vistas/ticket";
 		}
 
 	}
 
 	@PreAuthorize("hasAnyRole('ROLE_ADMINISTRADOR')")
 	@PostMapping("/registrar")
-	public String registrarTicket(ModelMap modelo, Ticket ticket) throws Exception {
+	public String registrar(ModelMap modelo, Ticket ticket) throws Exception {
 		try {
 			servicioTicket.crearTicket(ticket);
 			return "redirect:/ticket";
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
-			modelo.put("error", "Falta algun dato");
-			modelo.addAttribute("registrar", "Registrar Ticket");
-			modelo.addAttribute(ticket);
+			e.printStackTrace();
+			modelo.put("error", "Error al ingresar datos");
 			return "redirect:/ticket";
 		}
 	}
 
 	@PreAuthorize("hasAnyRole('ROLE_ADMINISTRADOR')")
 	@GetMapping("/editar/{id}")
-	public String modificarTicket(ModelMap modelo) throws Exception {
+	public String editar(ModelMap modelo) throws Exception {
 		try {
 			modelo.addAttribute("registrar", "Registrar Ticket");
 			modelo.addAttribute("ticket", servicioTicket.crearTicketVac());
@@ -91,25 +88,23 @@ public class TicketControlador {
 			modelo.addAttribute("usuarios", servicioUsuario.buscarUsuarios());
 			return "admin/vistas/ticket";
 		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			modelo.put("error", e.getMessage());
-			return "admin/vistas/ticket";
+			e.printStackTrace();
+			modelo.put("error", "Error al ingresar datos");
+			return "vistas/ticket";
 		}
 	}
 
 	@PreAuthorize("hasAnyRole('ROLE_ADMINISTRADOR')")
 	@PostMapping("editar/{id}")
-	public String modificarTicket(ModelMap modelo, Ticket ticket, @PathVariable Long id,
-			@RequestParam Pelicula pelicula, @RequestParam Usuario usuario, @RequestParam String fecha,
-			@RequestParam String lugar, @RequestParam Double precio) throws Exception {
+	public String editar(ModelMap modelo, Ticket ticket, @PathVariable Long id, @RequestParam Pelicula pelicula,
+			@RequestParam Usuario usuario, @RequestParam String fecha, @RequestParam String lugar,
+			@RequestParam Double precio) throws Exception {
 		try {
 			servicioTicket.modificarTicket(id, pelicula, usuario, fecha, lugar, precio);
 			return "redirect:/ticket";
 		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			modelo.put("error", "Falta algun dato");
-			modelo.addAttribute("registrar", "Registrar Ticketr");
-			modelo.addAttribute(ticket);
+			e.printStackTrace();
+			modelo.put("error", "Error al ingresar datos");
 			return "redirect:/ticket";
 		}
 
@@ -122,7 +117,7 @@ public class TicketControlador {
 			servicioTicket.eliminarTicket(idTicket);
 			return "redirect:/ticket";
 		} catch (Exception e) {
-			System.out.println(e.getMessage());
+			e.printStackTrace();
 			return "redirect:/ticket";
 		}
 	}

fe4c6b23272b0095ba6197535840371b53b6c2b2
Oriana <orilupellegrini@gmail.com>
2021-11-05 02:38:43 -0300

    Modifico controlador y servicio actor para implementar th:object

--- src/main/java/com/cinyema/app/controladores/ActorControlador.java
+++ src/main/java/com/cinyema/app/controladores/ActorControlador.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.cinyema.app.entidades.Actor;
 import com.cinyema.app.enumeraciones.Pais;
 import com.cinyema.app.servicios.ActorServicio;
@@ -24,9 +23,9 @@ public class ActorControlador {
 
 	@PreAuthorize("hasAnyRole('ROLE_ADMINISTRADOR')")
 	@GetMapping("")
-	public String lista(ModelMap modelo) {
+	public String listar(ModelMap modelo) {
 		modelo.addAttribute("listar", "Lista Actores");
-		modelo.addAttribute("actores", actorServicio.buscarActores());
+		modelo.addAttribute("actores", actorServicio.listar());
 		return "vistas/actor";
 	}
 
@@ -48,33 +47,31 @@ public class ActorControlador {
 		} catch (Exception e) {
 			e.printStackTrace();
 			modelo.put("error", "Error al ingresar datos");
+			modelo.addAttribute("registrar", "Registrar Actor");
+			actorServicio.registrar(actor);
 			return "redirect:/actor";
 		}
 	}
 
 	@PreAuthorize("hasAnyRole('ROLE_ADMINISTRADOR')")
 	@GetMapping("/editar/{id}")
-	public String editar(ModelMap modelo) throws Exception {
-		try {
+	public String editar(ModelMap modelo, @PathVariable Long idActor) throws Exception {
 			modelo.addAttribute("editar", "Editar Actor");
-			modelo.addAttribute("actor", actorServicio.registrarVacio();
+			modelo.addAttribute("actor", actorServicio.obtenerActorPorId(idActor));
 			return "vistas/actor";
-		} catch (Exception e) {
-			e.printStackTrace();
-			modelo.put("error", "Error al ingresar datos");
-			return "vistas/actor";
-		}
 	}
 
 	@PreAuthorize("hasAnyRole('ROLE_ADMINISTRADOR')")
 	@PostMapping("/editar/{id}")
 	public String editar(ModelMap modelo, Actor actor) throws Exception {
 		try {
-			actorServicio.modificarActor(actor);
+			actorServicio.editar(actor);
 			return "redirect:/actor";
 		} catch (Exception e) {
 			e.printStackTrace();
+			modelo.addAttribute("editar", "Editar Actor");
 			modelo.addAttribute("error", "Error al ingresar datos");
+			actorServicio.editar(actor);
 			return "redirect:/actor" ;
 		}
 		
@@ -84,7 +81,7 @@ public class ActorControlador {
 	@GetMapping("/eliminar/{id}")
 	public String eliminar(@PathVariable Long id) {
 		try {
-			actorServicio.eliminarDirector(id);
+			actorServicio.eliminar(id);
 			return "redirect:/actor";
 		} catch (Exception e) {
 			e.printStackTrace();

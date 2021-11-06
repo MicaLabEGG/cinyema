ff8ed822ad7fb3c629968be6e9c3e502c15e9464
Oriana <orilupellegrini@gmail.com>
2021-11-05 21:54:19 -0300

    Arreglo acceso a lista pelicula
    arreglo rutas de coltrolador, ruta del formulario de registroPelicula, ruta botón registrar pelicula en lista de peliculas y ícono de eliminar en listar peliculas

--- src/main/java/com/cinyema/app/controladores/PeliculaControlador.java
+++ src/main/java/com/cinyema/app/controladores/PeliculaControlador.java
@@ -11,10 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import com.cinyema.app.entidades.Pelicula;
-import com.cinyema.app.enumeraciones.Genero;
-import com.cinyema.app.enumeraciones.Idioma;
-import com.cinyema.app.enumeraciones.Pais;
-import com.cinyema.app.enumeraciones.Subtitulo;
 import com.cinyema.app.servicios.ActorServicio;
 import com.cinyema.app.servicios.DirectorServicio;
 import com.cinyema.app.servicios.PeliculaServicio;
@@ -39,7 +35,7 @@ public class PeliculaControlador {
 		modelo.addAttribute("listar", "Lista de Películas");
 		modelo.addAttribute("peliculas", servicioPelicula.listar());
 		modelo.addAttribute("actores", servicioActor.listar());
-		return "vistas/pelicula";
+		return "vistas/admin/pelicula";
 	}
 
 	@PreAuthorize("hasAnyRole('ROLE_ADMINISTRADOR')")
@@ -49,7 +45,7 @@ public class PeliculaControlador {
 		modelo.addAttribute("directores", servicioDirector.listar());
 		modelo.addAttribute("actores", servicioActor.listar());
 		modelo.addAttribute("pelicula", servicioPelicula.registrarVacio());
-		return "vistas/pelicula";
+		return "vistas/admin/pelicula";
 	}
 
 	@PreAuthorize("hasAnyRole('ROLE_ADMINISTRADOR')")
@@ -73,7 +69,7 @@ public class PeliculaControlador {
 	public String editar(ModelMap modelo, @PathVariable Long idPelicula) {
 		modelo.addAttribute("editar", "Editar Películas");
 		modelo.addAttribute("pelicula", servicioPelicula.obtenerPeliculaPorId(idPelicula));
-		return "vistas/pelicula";
+		return "vistas/admin/pelicula";
 	}
 
 	@PreAuthorize("hasAnyRole('ROLE_ADMINISTRADOR')")
@@ -123,13 +119,13 @@ public class PeliculaControlador {
 		try {
 			modelo.addAttribute("buscador", "Buscador de películas por Título");
 			modelo.addAttribute("peliculas", servicioPelicula.obtenerPeliculaPorTitulo(titulo));
-			return "vistas/pelicula";
+			return "vistas/admin/pelicula";
 		} catch (Exception e) {
 			e.printStackTrace();
 			modelo.addAttribute("editar", "Editar Película");
 			modelo.addAttribute("pelicula", servicioPelicula.obtenerPeliculaPorTitulo(titulo));
 			modelo.put("error", e.getMessage());
-			return "vistas/pelicula";
+			return "vistas/admin/pelicula";
 		}
 	}
 
@@ -141,6 +137,7 @@ public class PeliculaControlador {
 			return "redirect:/pelicula";
 		} catch (Exception e) {
 			e.printStackTrace();
+			System.err.println(e.getMessage());
 			return "redirect:/pelicula";
 		}
 

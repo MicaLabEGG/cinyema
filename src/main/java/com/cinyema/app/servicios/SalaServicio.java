fcfb23930de681b683a9f2c93e49a005a231cbf0
Micael Labria <micaelcapo1316@gmail.com>
2021-11-07 19:50:45 -0300

    Le agregue la Interface de metodos a SalaServicio

--- src/main/java/com/cinyema/app/servicios/SalaServicio.java
+++ src/main/java/com/cinyema/app/servicios/SalaServicio.java
@@ -12,23 +12,22 @@ import com.cinyema.app.entidades.Sala;
 import com.cinyema.app.repositorios.SalaRepositorio;
 
 @Service
-public class SalaServicio {
+public class SalaServicio implements ServicioBase<Sala>{
 
 	@Autowired
 	SalaRepositorio salaRepositorio;
 
+	@Override
 	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = { Exception.class })
-	public Sala crearSala(Sala sala) throws Exception {
-
+	public Sala registrar(Sala sala) throws Exception {
 		validar(sala);
-
 		return salaRepositorio.save(sala);
 	}
 
+	@Override
 	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = { Exception.class })
-	public Sala modificarSala(Sala sala) throws Exception {
+	public Sala editar(Sala sala) throws Exception {
 		validar(sala);
-
 		Optional<Sala> respuesta = salaRepositorio.findById(sala.getIdSala());
 		if (respuesta.isPresent()) {
 			Sala sala1 = respuesta.get();
@@ -44,36 +43,39 @@ public class SalaServicio {
 	}
 
 	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = { Exception.class })
-	public void eliminarSala(Sala sala) {
+	public void eliminarPorEntidad(Sala sala) {
 		salaRepositorio.delete(sala);
 	}
 
+	@Override
 	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = { Exception.class })
-	public void elminarSalaPorId(Long id) {
+	public void eliminar(Long id) {
 		salaRepositorio.deleteById(id);
 	}
 
+	@Override
 	@Transactional(readOnly = true)
-	public List<Sala> listarSalas() {
-
+	public List<Sala> listar() {
 		return salaRepositorio.findAll();
 	}
 
-	@Transactional(readOnly = true)
-	public Optional<Sala> buscarSalaPorId(Long id) {
-		return salaRepositorio.findById(id);
+	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = { Exception.class })
+	public Sala obtenerPorId(Long id) {
+		return salaRepositorio.getById(id);
+	}
+	
+	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = { Exception.class })
+	public Sala obtenerSalaPorTituloPelicula(String titulo) {
+		return salaRepositorio.buscarSalaPorPelicula(titulo);
 	}
 
 	public void validar(Sala sala) throws Exception {
-
 		if (sala.getAsientos() == null) {
 			throw new Exception("El asiento es inexistente");
 		}
-
+		
 		if (sala.getPelicula() == null) {
 			throw new Exception("La Pelicula es inexistente");
 		}
-
 	}
-
 }

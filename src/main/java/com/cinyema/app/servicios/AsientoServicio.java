43e810a0f081d15b83b0c13defaa9d5c39249779
Micael Labria <micaelcapo1316@gmail.com>
2021-11-07 20:27:17 -0300

    Le puse @Servicio a AsientoServicio

--- src/main/java/com/cinyema/app/servicios/AsientoServicio.java
+++ src/main/java/com/cinyema/app/servicios/AsientoServicio.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.cinyema.app.entidades.Asiento;
 import com.cinyema.app.repositorios.AsientoRepositorio;
 
+@Service
 public class AsientoServicio {
 	
 	@Autowired

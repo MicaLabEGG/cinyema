e97a17c6468fb0f4c457a4489b9a386d0497ca6a
Micael Labria <micaelcapo1316@gmail.com>
2021-11-16 11:13:12 -0300

    Ahora si le va a pasar los asientos solamente libres

--- src/main/java/com/cinyema/app/controladores/TicketControlador.java
+++ src/main/java/com/cinyema/app/controladores/TicketControlador.java
@@ -145,8 +145,7 @@ public class TicketControlador {
 			modelo.addAttribute("pelicula", servicioPelicula.obtenerPorId(idPelicula));
 			modelo.addAttribute("sala", servicioPelicula.obtenerSalaPorIdPelicula(idPelicula));
 			Sala sala = servicioPelicula.obtenerSalaPorIdPelicula(idPelicula);
-			//salaServicio.obtenerAsientosLibre(sala);
-			modelo.addAttribute("asientos", sala.getAsientos());
+			modelo.addAttribute("asientos", salaServicio.obtenerAsientosLibres(sala));
 			Ticket ticket = servicioTicket.registrarVacio();
 			ticket.setUsuario(servicioUsuario.obtenerUsuarioPorNombre(autenticacion.getName()));
 			ticket.setPelicula(servicioPelicula.obtenerPorId(idPelicula));

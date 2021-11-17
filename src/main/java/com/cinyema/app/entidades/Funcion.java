c44a49bd4c78b19c3aae764104631e186816ea53
Micael Labria <micaelcapo1316@gmail.com>
2021-11-17 20:38:00 -0300

    Le saque el Data y le puse Get y Setters , Contructores y ToString, por recursion

--- src/main/java/com/cinyema/app/entidades/Funcion.java
+++ src/main/java/com/cinyema/app/entidades/Funcion.java
@@ -16,7 +16,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
 
-@Data
 @Entity
 public class Funcion {
 	
@@ -33,8 +32,74 @@ public class Funcion {
 	@OneToMany(mappedBy = "funcion")
 	private List<Ticket> tickets;
 	
+	public Funcion(Long idFuncion, String fecha, String horario, Pelicula pelicula, Sala sala, List<Ticket> tickets) {
+		super();
+		this.idFuncion = idFuncion;
+		this.fecha = fecha;
+		this.horario = horario;
+		this.pelicula = pelicula;
+		this.sala = sala;
+		this.tickets = tickets;
+	}
 
-	
+	public Funcion() {
+		super();
+	}
+
+	public Long getIdFuncion() {
+		return idFuncion;
+	}
+
+	public void setIdFuncion(Long idFuncion) {
+		this.idFuncion = idFuncion;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getHorario() {
+		return horario;
+	}
+
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+
+	public Pelicula getPelicula() {
+		return pelicula;
+	}
+
+	public void setPelicula(Pelicula pelicula) {
+		this.pelicula = pelicula;
+	}
+
+	public Sala getSala() {
+		return sala;
+	}
+
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+
+	public List<Ticket> getTickets() {
+		return tickets;
+	}
+
+	public void setTickets(List<Ticket> tickets) {
+		this.tickets = tickets;
+	}
+
+	@Override
+	public String toString() {
+		return "Funcion [idFuncion=" + idFuncion + ", fecha=" + fecha + ", horario=" + horario + ", pelicula="
+				+ pelicula + ", tickets=" + tickets + "]";
+	}
+
 	public Long randomId() {
 		String uuid = UUID.randomUUID().toString();
 		Long id = (long) uuid.hashCode();

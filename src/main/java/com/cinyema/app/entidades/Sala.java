f3d3fa2c7ee1aa902112e6eb21718ae3796345f0
Micael Labria <micaelcapo1316@gmail.com>
2021-11-18 03:14:18 -0300

    Le borre atributo precio a Ticket y borre la linea comentada de fecha
    Y borre la linea comentada de atributo pelicula en Sala

--- src/main/java/com/cinyema/app/entidades/Sala.java
+++ src/main/java/com/cinyema/app/entidades/Sala.java
@@ -22,8 +22,6 @@ public class Sala {
 
 	@Id
 	private Long idSala = randomId();
-//	@OneToOne
-//	private Pelicula pelicula;
 	@OneToMany(cascade = CascadeType.ALL)
 	@JoinColumn(name = "sa_fid", referencedColumnName = "idSala" )
 	private List<Asiento> asientos;
